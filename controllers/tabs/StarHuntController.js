c5108737d71e6863f0e34bfd047a1b0fd543d7d1
Peter Williams <peter@newton.cx>
2020-01-30 17:09:27 -0500

    Wire up an opacity control for StarHunt targets

--- controllers/tabs/StarHuntController.js
+++ controllers/tabs/StarHuntController.js
@@ -11,7 +11,7 @@
     'UILibrary',
 
     function ($scope, $rootScope, appState, places, $timeout, util, thumbList, uiLib) {
-      // A dumb, fake Place workalike for our custom
+      // A dumb, fake Place workalike for our custom app
 
       function StarHuntItem(name, fits_url) {
         var item = Object.create(StarHuntItem.prototype);
@@ -40,6 +40,8 @@
       }
 
       StarHuntItem.prototype.custom_click_action = function () {
+        maybe_init_controls();
+
         if (this._fits_layer == null) {
           // We need to start loading the FITS layer.
           this._fits_layer = wwt.wc.loadFitsLayer(
@@ -50,6 +52,8 @@
               layer.getFitsImage().transparentBlack = false;
             }
           );
+
+          $("#starhunt-opacity").val(100);
         } else {
           // FITS layer loaded or loading. Steer the UI there.
           var fits = this._fits_layer.getFitsImage();
@@ -62,7 +66,11 @@
               false
             );
           }
+
+          $("#starhunt-opacity").val(this._fits_layer.get_opacity() * 100);
         }
+
+        current_item = this;
       }
 
       StarHuntItem.prototype.get_isFolder = function () {
@@ -89,7 +97,43 @@
         return null;
       }
 
-      // Actual UI logic
+      // Defining the set of targets
+
+      var starhunt_items = [
+        new StarHuntItem("Source A", "/starhunt_data/A_J2000_msd_header_crop.fits"),
+        new StarHuntItem("Source B", "/starhunt_data/B_J2000_msd_header_crop.fits")
+      ];
+
+      // Image controls
+
+      var current_item = null,
+          controls_initialized = false;
+
+      function maybe_init_controls() {
+        if (controls_initialized) {
+          return;
+        }
+
+        var opacity_dom = $("#starhunt-opacity");
+        // Need to watch the "input" event to get changes before mouse-up:
+        opacity_dom.on('input change', function () { on_opacity_changed(opacity_dom) });
+
+        controls_initialized = true;
+      }
+
+      function on_opacity_changed(opacity_dom) {
+        if (current_item == null) {
+          return;
+        }
+
+        if (current_item._fits_layer == null) {
+          return;
+        }
+
+        current_item._fits_layer.set_opacity(0.01 * opacity_dom.val());
+      }
+
+      // Thumbnail list UI logic
 
       var depth = 1,
           bc,
@@ -97,28 +141,12 @@
           openCollection,
 	  newCollectionUrl;
 
-      $scope.initStarHuntView = function () {
-	thumbList.init($scope, 'starhunt');
-
-	$('body').append($('#researchMenu'));
-
-	$scope.collection = [
-          new StarHuntItem("Source A", "/starhunt_data/A_J2000_msd_header_crop.fits"),
-          new StarHuntItem("Source B", "/starhunt_data/B_J2000_msd_header_crop.fits")
-        ];
-
-	$scope.breadCrumb = bc = ['Targets'];
-
-	cache = [$scope.collection];
-	calcPageSize();
-      };
-
       var calcPageSize = function () {
 	thumbList.calcPageSize($scope, false);
       };
 
       $scope.clickThumb = function (item) {
-	var out_params = {
+	var cur_params = {
 	  breadCrumb: bc,
           depth: depth,
 	  cache: cache,
@@ -126,7 +154,8 @@
 	  newCollectionUrl: newCollectionUrl
 	};
 
-	var new_params = thumbList.clickThumb(item, $scope, out_params, null);
+	var new_params = thumbList.clickThumb(item, $scope, cur_params, null);
+
 	bc = new_params.breadCrumb;
 	cache = new_params.cache;
 	openCollection = new_params.openCollection;
@@ -141,7 +170,18 @@
 	calcPageSize();
       });
 
-      $scope.initStarHuntView();
+      function init_thumbnail_ui() {
+	thumbList.init($scope, 'starhunt');
+	$scope.collection = starhunt_items;
+	$scope.breadCrumb = bc = ['Targets'];
+	cache = [$scope.collection];
+	calcPageSize();
+      }
+
+      // Final initialization
+
+      $('body').append($('#researchMenu'));
+      init_thumbnail_ui();
     }
   ]
 );
